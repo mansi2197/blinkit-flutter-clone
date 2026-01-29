bab8f2a9e35a193daf8f6697d7d0598701ab98f4
Mansi Parikh <144649215+mansi2197@users.noreply.github.com>
2026-01-29 13:03:59 +0530

    Initial Flutter project withsplash screen

---
+++ lib/repository/screens/splash/splashscreen.dart
@@ -0,0 +1,25 @@
+import 'package:blinkit_clone/domain/constants/appcolors.dart';
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget{
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Uihelper.CustomImage(img: "blinkitlogo.png"),
+        ],),
+      ),
+    );
+
+  }
+}
